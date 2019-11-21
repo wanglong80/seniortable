477786a45006c820004acc9421a83c8c6967e3ac
wanglong <master@wanglong.name>
2019-11-21 23:51:09 +0800

    修复粘贴表格在不同换行符下的问题

--- src/plugins/paste.js
+++ src/plugins/paste.js
@@ -1,38 +1,38 @@
 /* global window */
 import { bind } from '../component/event';
 
-function charFilter(str) {
-  let fileType = '';
-  // 非可见字符asc,多个就可以用数组
-  const ascNum = 173;
-  for (let i = 0; i < str.length; i++) {
-    console.log(`${str.charAt(i)}:${str.charCodeAt(i)}`);
-    if (str.charCodeAt(i) !== ascNum) {
-      fileType += str.charAt(i);
-    }
-  }
-  return fileType;
-}
-
-
 // 粘贴插件
 export default class Plugin {
   setup() {
     bind(window, 'paste', (e) => {
       let text = e.clipboardData.getData('text');
-      //  text = charFilter(text);
-
-      console.log(text);
       text = encodeURIComponent(text);
-      console.log(text);
-
 
       const axis = this.getSelectedAxis();
       const sri = axis.ri;
       const sci = axis.ci;
 
       // 得到粘贴板内容的数组
-      const rowsArray = text.split('%0D%0A'); // 换行符
+      const rowsArray1 = text.split('%0D%0A');
+      const rowsArray2 = text.split('%0A');
+      let rowsArray = [];
+
+      if (rowsArray1.length > 1) {
+        if (rowsArray1[0].split('%09').length === rowsArray1[1].split('%09').length) {
+          rowsArray = rowsArray1;
+        }
+      } else {
+        rowsArray = rowsArray1;
+      }
+
+      if (rowsArray2.length > 1) {
+        if (rowsArray2[0].split('%09').length === rowsArray2[1].split('%09').length) {
+          rowsArray = rowsArray2;
+        }
+      } else {
+        rowsArray = rowsArray2;
+      }
+
       if (rowsArray.length > 0) {
         // 从excel 复制的数据最后一行会有空的回车符，这里要手动的去掉
         if (rowsArray[rowsArray.length - 1].length === 0) {
@@ -42,7 +42,6 @@ export default class Plugin {
         for (let ri = 0; ri < rowsArray.length; ri += 1) {
           let rowText = rowsArray[ri];
           rowText = rowText.replace(/(?:%22)((?:%22)*)/ig, '$1');
-
 
           const colsArray = rowText.split('%09'); // 间隔符
 
