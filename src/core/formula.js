91c96d66a7b2488eb8276d1d19d6cc14c5f24e92
wanglong <master@wanglong.name>
2019-11-25 20:27:34 +0800

    update

--- src/core/formula.js
+++ src/core/formula.js
@@ -1,57 +1,34 @@
-/**
-  formula:
-    key
-    title
-    render
-*/
-/**
- * @typedef {object} Formula
- * @property {string} key
- * @property {function} title
- * @property {function} render
- */
-import { tf } from '../locale/locale';
-
-/** @type {Formula[]} */
 const baseFormulas = [
   {
     key: 'SUM',
-    title: tf('formula.sum'),
     render: ary => ary.reduce((a, b) => Number(a) + Number(b), 0),
   },
   {
     key: 'AVERAGE',
-    title: tf('formula.average'),
     render: ary => ary.reduce((a, b) => Number(a) + Number(b), 0) / ary.length,
   },
   {
     key: 'MAX',
-    title: tf('formula.max'),
     render: ary => Math.max(...ary.map(v => Number(v))),
   },
   {
     key: 'MIN',
-    title: tf('formula.min'),
     render: ary => Math.min(...ary.map(v => Number(v))),
   },
   {
     key: 'IF',
-    title: tf('formula._if'),
     render: ([b, t, f]) => (b ? t : f),
   },
   {
     key: 'AND',
-    title: tf('formula.and'),
     render: ary => ary.every(it => it),
   },
   {
     key: 'OR',
-    title: tf('formula.or'),
     render: ary => ary.some(it => it),
   },
   {
     key: 'CONCAT',
-    title: tf('formula.concat'),
     render: ary => ary.join(''),
   },
   /* support:  1 + A1 + B2 * 3
